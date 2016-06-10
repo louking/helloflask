a9359a4e00ed542faba5e273aa78103bbc3b6966
Lou King <lking@pobox.com>
2016-06-10 09:23:09 -0400

    Several updates needed to make fab deply work

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import env, run, cd
 
 USERNAME = 'scoretility'
-SERVER = 'hello.scoretility.org'
+SERVER = 'hello.scoretility.com'
 APP_NAME = 'helloflask'
 PROJECT_DIR = '/var/www/%s/%s' % (SERVER, APP_NAME)
 WSGI_SCRIPT = 'application.wsgi'
@@ -11,6 +11,5 @@ env.hosts = ["%s@%s" % (USERNAME, SERVER)]
 def deploy():
     with cd(PROJECT_DIR):
         run('git pull')
-        run('bin source/activate')
-        run('pip install -r requirements.txt')
+        run('source bin/activate; pip install -r requirements.txt')
         run('touch %s' % WSGI_SCRIPT)
