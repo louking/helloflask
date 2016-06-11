6fbc48b0b088239c41bd1aa0de7e0af7588c13ad
Lou King <lking@pobox.com>
2016-06-11 06:24:17 -0400

    test cfg [app] section

--- helloflask.py
+++ helloflask.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from ConfigParser import SafeConfigParser
+
 app = Flask(__name__)
+
+# get configuration
+config = SafeConfigParser()
+thisdir = os.path.dirname(__file__)
+parentdir = '/'.join(thisdir.split('/')[:-1])
+config.readfp(open(os.path.join(parentdir, 'helloflask.cfg')))
+appconfig = dict(config.items('app'))
 
 @app.route("/")
 def hello():
-    return "Hello World! (updated)"
+    return "Hello World!\n{}".format(appconfig)
 
 if __name__ == "__main__":
     app.run()
